a16a4452e8266e17dcda893488fc4532cd00d0ed
Fitria Rahmaani <fitria.rahmaani@gmail.com>
2020-04-07 13:09:44 +0700

    Setup devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :initialize_user
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :user_name, :shop_name, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :user_name, :shop_name, :email, :password, :current_password)}
+  end
+
+  def initialize_user
+    @user = User.new
+  end
+
 end
